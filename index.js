1c84d9ff17b44b69365fe4a969296ad3ca828130
mo-ay <mostafaaanezan@gmail.com>
2020-10-23 14:50:16 +0300

    step 4

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3001
 
 app.get('/', (req, res) => {
   res.send('ok')
@@ -14,6 +14,21 @@ app.get('/time', (req, res) => {
     message = "status :200 , message: " + hour + " : " +  second
     res.send(message)
 })
+
+app.get('/hello/:ID', (req, res) => {
+    res.send('status :200 , message: "hello" ,' + req.params.ID)
+})
+app.get('/hello', (req, res) => {
+    res.send('status :200 , message: "hello" ,' )
+})
+app.get('/search', (req, res) => {
+    if(req.query.s){
+        res.send('status :200 , message: "ok" , data : ' +req.query.s)
+    }else{
+        res.send('status:500, error:true, message:"you have to provide a search"')
+    }
+})
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
