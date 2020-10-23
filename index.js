e3efad6364713ebe8ddb7a3a7e9b346ce6698a00
mo-ay <mostafaaanezan@gmail.com>
2020-10-23 13:55:53 +0300

    Step 3

--- index.js
+++ index.js
@@ -5,7 +5,15 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send('ok')
 })
-
+app.get('/test', (req, res) => {
+    res.send("status :200 , message: 'ok'")
+})
+app.get('/time', (req, res) => {
+    hour = new Date().getHours();
+    second = new Date().getSeconds();
+    message = "status :200 , message: " + hour + " : " +  second
+    res.send(message)
+})
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
